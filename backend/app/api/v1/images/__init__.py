aa82986a076fbb3f6ebaeea7c75dd362945b8cda
afei A <57030625+NewHubBoy@users.noreply.github.com>
2026-01-26 16:02:38 +0800

    fix text error

--- backend/app/api/v1/images/__init__.py
+++ backend/app/api/v1/images/__init__.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from .images import router
 
 images_router = APIRouter()
-images_router.include_router(router, tags=["图片模块"])
+images_router.include_router(router, tags=["图片生成模块"])
 
 __all__ = ["images_router"]
--- backend/app/api/v1/model_photo/__init__.py
+++ backend/app/api/v1/model_photo/__init__.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from .model_photo import router
 
 model_photo_router = APIRouter()
-model_photo_router.include_router(router, prefix="/model_photo", tags=["换装合成模块"])
+model_photo_router.include_router(router, prefix="/model_photo", tags=["模特管理模块"])
 
 __all__ = ["model_photo_router"]
